9ee662dca5d7396bba35e2a4dd3c77c67f8fa6bb
huangxiaofeng <hxf168482@gmail.com>
2022-12-05 11:16:37 +0800

    feat(n): helm
    helm

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 echo -n "Starting Kubernetes..."
-export NO_PROXY=localhost,127.0.0.1,10.96.0.0/12,192.168.59.0/24,192.168.49.0/24,192.168.39.0/24
 minikube version
-# minikube start --wait=false --image-repository='registry.cn-hangzhou.aliyuncs.com/google_containers'  --image-mirror-country='cn'
-minikube start --memory=8192 --cpus=4     --image-mirror-country='cn'  
+minikube start --memory=6192 --cpus=4     --image-mirror-country='cn'  
 sleep 2
 n=0
 until [ $n -ge 10 ]
